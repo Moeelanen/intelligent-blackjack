f4213542a7842496d668f0516edfbacaabff047f
Moeelanen <miikka.moilanen@outlook.com>
2021-11-08 00:09:26 +0200

    Improved the naming scheme

--- dealer_server/manage.py
+++ dealer_server/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blackjack_server.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dealer_server.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
