4113917566f3230fbe872078ad491453b6fc051b
Moeelanen <miikka.moilanen@outlook.com>
2021-12-17 20:27:49 +0200

    Dataset revision and files for the 3D model

--- ML/dataset-generator.py
+++ ML/dataset-generator.py
@@ -4,11 +4,15 @@ from PIL import Image
 
 for file in os.listdir("images/"):
     file_array = file.split("_")
-    directory = "dataset/" + file_array[0] + "/"
+    test_directory = "dataset/test/" + file_array[0] + "/"
+    train_directory = "dataset/train/" + file_array[0] + "/"
     name = file_array[1]
     
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    if not os.path.exists(test_directory):
+        os.makedirs(test_directory)
+    
+    if not os.path.exists(train_directory):
+        os.makedirs(train_directory)
         
     im = Image.open(r"images/" + file)
     width, height = im.size
@@ -21,4 +25,7 @@ for file in os.listdir("images/"):
     im_crop = im.crop((left, top, right, bottom))
 
     print(name, im_crop.size)
-    im_crop.save(directory + name)
+    if (int(name.split(".")[0]) > 25):
+        im_crop.save(test_directory + name)
+    else:
+        im_crop.save(train_directory + name)
